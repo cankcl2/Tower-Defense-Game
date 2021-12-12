4929796eb50bac7ed764fc4db2775a76f2fa1628
cankcl2 <80987102+cankcl2@users.noreply.github.com>
2021-12-12 09:36:11 +0300

    Improving Environment

---
+++ Tower Defense/Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f,5f)] float speed = 1f;
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+        
+    }
+
+    // Update is called once per frame
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            Vector3 startPos = transform.position;
+            Vector3 endPos = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPos);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPos, endPos, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+            
+        }
+    }
+}
